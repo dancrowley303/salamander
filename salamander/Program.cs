647bdebc40979a911cccc0d257a54ea13942881e
Dan Crowley <defrobo@outlook.com>
2017-11-03 10:41:40 +0900

    Add project files.

---
+++ salamander/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salamander
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
